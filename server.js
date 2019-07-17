8c9410d12fc71b399370fe39483f4d11bc9de954
David <davwil38@gmail.com>
2019-07-17 08:20:20 -0400

    mongodb connection
    -added server code for mongo db connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+var mongoose = require("mongoose");
+//var db = require("./models");
 const path = require("path");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/LanguageApp";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
