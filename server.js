17354f57105d3c5d003aaf80ac5fe21a41110b43
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-08-04 18:38:51 -0400

    added pseudocode

--- server.js
+++ server.js
@@ -156,3 +156,21 @@ io.on('connection', (socket) => {
     });
 })
 // ===== END SOCKET.IO ==================================================== //
+
+// const {JWT} = require('google-auth-library');
+// const keys = require('../../GCP/ExploratoryProject1-a429df58be31.json');
+// // const keys = require('./jwt.keys.json');
+
+// async function main() {
+//   const client = new JWT(
+//     keys.client_email,
+//     null,
+//     keys.private_key,
+//     ['https://www.googleapis.com/auth/cloud-platform'],
+//   );
+//   const url = `https://www.googleapis.com/dns/v1/projects/${keys.project_id}`;
+//   const res = await client.request({url});
+//   console.log(res.data);
+// }
+
+// main().catch(console.error);
