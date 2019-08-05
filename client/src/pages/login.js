e7f4d89ce87c6c212e33f35be26821a43a5b1599
John Robertson <tiktaalikdesign@gmail.com>
2019-08-04 21:06:38 -0400

    user demos on login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import Box from "@material-ui/core/Box";
 import LoginForm from "../components/loginform";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
 import NavButton from "../components/navigationbutton";
 import Button from '@material-ui/core/Button';
 
 class LoginPage extends Component {
 
     // AJAX request to fetch User info from dB
-
 
     // pass user info from db into updateParent function
     updateParent = () => {
@@ -25,6 +22,40 @@ class LoginPage extends Component {
         // then once state is updated we re-route to messenger page
     };
 
+    // USER DEMOS
+    updateJohn = () => {
+        let name = 'John';
+        let native = 'en';
+        let target = 'es';
+        let email = 'evilevilmonkey@familyguy.com';
+        let id = 128922;
+        this.props.appStateHandler(name, native, target, email, id);
+    };
+    updateDavid = () => {
+        let name = 'David';
+        let native = 'en';
+        let target = 'it';
+        let email = 'evilevilmonkey@familyguy.com';
+        let id = 128942;
+        this.props.appStateHandler(name, native, target, email, id);
+    };
+    updateChaney = () => {
+        let name = 'Chaney';
+        let native = 'en';
+        let target = 'fr';
+        let email = 'evilevilmonkey@familyguy.com';
+        let id = 128945;
+        this.props.appStateHandler(name, native, target, email, id);
+    };
+    updateMarshall = () => {
+        let name = 'Marshall';
+        let native = 'en';
+        let target = 'ru';
+        let email = 'evilevilmonkey@familyguy.com';
+        let id = 128949;
+        this.props.appStateHandler(name, native, target, email, id);
+    };
+
     render() {
         console.log(this.props)
         return (
@@ -32,6 +63,10 @@ class LoginPage extends Component {
                 <LoginForm />
 
                 <Button onClick={this.updateParent}>Update</Button>
+                <Button onClick={this.updateJohn}>John</Button>
+                <Button onClick={this.updateDavid}>David</Button>
+                <Button onClick={this.updateChaney}>Chaney</Button>
+                <Button onClick={this.updateMarshall}>Marshall</Button>
                 <NavButton to="/messenger" text="Start Chatting!" />
             </Box>
         );
