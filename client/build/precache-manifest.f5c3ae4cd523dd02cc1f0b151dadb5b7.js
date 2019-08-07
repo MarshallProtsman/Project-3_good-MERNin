f891ea091c33c75da67108ac172b0f590d0b0aa8
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-08-07 17:44:41 -0400

    heroku 5

---
+++ client/build/precache-manifest.f5c3ae4cd523dd02cc1f0b151dadb5b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d26f302abc7e74a46ac5eeb4b8ed510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49aca1abcaaa7c050dd1",
+    "url": "/static/css/2.d7b80fd4.chunk.css"
+  },
+  {
+    "revision": "a4d3934eaa6ce5d32e1f",
+    "url": "/static/css/main.4c77efab.chunk.css"
+  },
+  {
+    "revision": "49aca1abcaaa7c050dd1",
+    "url": "/static/js/2.26afdbee.chunk.js"
+  },
+  {
+    "revision": "a4d3934eaa6ce5d32e1f",
+    "url": "/static/js/main.91651cc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
