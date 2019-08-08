c8445989d8cfab5ed17a9747428e50291dc65341
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-08-07 21:40:45 -0400

    heroku 10

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ app.delete("/messenger", function (req, res) {
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
